8b8a21de23a186b98c041903f3fd64035e99a248
Cristian Fonseca <cristiancomas@hotmail.com>
2021-06-11 00:29:09 -0700

    Finished consume of API and show of data

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,51 @@ import WeatherInfo from './components/WeatherInfo'
 import WeatherForm  from './components/WeatherForm';
 import  { WEATHER_KEY } from './key'
 class App extends Component {
+    state = {
+        temperature: '',
+        description: '',
+        humidity: '',
+        wind_speed: '',
+        city: '',
+        country: '',
+        error: null,
+    }
     getWeather = async  e => {
+        // Previene que se envien los datos por defectos
         e.preventDefault()
+        // Capturar los datos que introduce el usuario
         const { city, country } = e.target.elements
         const cityValue = city.value
         const countryValue = country.value
+        if (cityValue && countryValue ) {
+            // Haciendo una peticion a la API, importando la WEATHER_KEY desde key.jsx
+            const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`
+            const response = await fetch(API_URL)
+            const data = await response.json()
+            
+            this.setState({
+                temperature: data.main.temp,
+                description: data.weather[0].description,
+                humidity: data.main.humidity,
+                wind_speed: data.wind.speed,
+                city: data.name,
+                country: data.sys.country,
+                error: null,
 
-        const API_URL = `api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}`
-        const response = await fetch(API_URL, )
-        const data =  JSON.stringify(response)
-        const res = JSON.parse(data)
-        console.log(res)
+            })
+        } else {
+            this.setState({error: 'Please enter a city and country'})
+        }
+        
     }
+
     render(){
         return (
             <div className="container p-4">
                 <div className="row">
                     <div className="col-md-4 mx-auto ">
                         <WeatherForm getWeather={this.getWeather}/>
-                        <WeatherInfo/>
+                        <WeatherInfo {...this.state}/>
                     </div>
                 </div>
             </div>
--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -3,8 +3,36 @@ import React from 'react'
 const WeatherInfo = (props) => {
     return (
         <div>
-            WeatherInfo
+            {
+                props.error &&
+                <div className="alert alert-danger">
+                    <p>{props.error}</p>
+                </div>
+            }
+            {
+                props.temperature ?
+                    <div className="card card-info">
+                        <p>
+                            Location: {props.city}, {props.country}
+                        </p>
+                        <p>
+                            Temperature: {props.temperature} °C, {props.description}
+                        </p>
+                        <p>
+                            Humidity: {props.humidity}
+                        </p>
+                        <p>
+                            Wind Speed: {props.wind_speed}
+                        </p>
+                    </div>
+                :
+                <div className="card card-info">
+                    <p>No request yet</p>
+                </div>
+            }
+
         </div>
+
     )
 }
 
