1791b2f0c8e1206a415005dcf2cdc9f35ea398f6
Cristian Fonseca <cristiancomas@hotmail.com>
2021-06-16 23:14:48 -0700

    Visual Changes

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -11,23 +11,18 @@ const WeatherInfo = (props) => {
             }
             {
                 props.temperature ?
-                    <div className="card card-info mt-1 text-center bg-transparent border-light">
-                        <p>
-                            Location: {props.city}, {props.country}
-                        </p>
-                        <p>
-                            Temperature: {props.temperature} °C, {props.description}
-                        </p>
-                        <p>
-                            Humidity: {props.humidity}
-                        </p>
-                        <p>
-                            Wind Speed: {props.wind_speed}
-                        </p>
+                    <div className="card card-info mt-1 text-center bg-light">
+                        <img src="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2019/01/weather-app.png"/>
+                        <div className="card-body text-black">
+                            <h4 className="card-title">{props.city}, {props.country}</h4>
+                            <p className="card-text text-primary">Temperature: {props.temperature} °C, {props.description} </p>
+                            <p className="card-text text-secondary">Humidity: {props.humidity}</p>
+                            <p className="card-text text-secondary">Wind Speed: {props.wind_speed}</p>
+                        </div>
                     </div>
                 :
-                <div className="card card-info mt-2 bg-transparent border-light">
-                    <p>No request yet</p>
+                <div className="card card-info mt-2 bg-light">
+                    <p className="card-text text-primary text-center">No request yet</p>
                 </div>
             }
 
